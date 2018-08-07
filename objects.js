3a3083d5fb68e3e3ff56ed191cc1b81782f07acd
Colette Mahoney <colettemahoney@hotmail.com>
2018-08-07 14:41:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 var playlist = {['Slowdive']: 'Alison',
+=======
+var playlist = {['Slowdrive']: 'Alison',
+>>>>>>> 0e1a8c29de620ccdf6acefcd5489c23597db9acc
 ['My Bloody Valentine']: 'Sometimes'};
 function updatePlaylist(playlist, artistName, songTitle){
  playlist['Phil Ochs'] = 'Here/s to the State of Mississippi';
 playlist
 }
 function removeFromPlaylist(playlist, artistName){
+<<<<<<< HEAD
   delete playlist.Slowdive;
+=======
+  delete playlist.Slowdrive;
+>>>>>>> 0e1a8c29de620ccdf6acefcd5489c23597db9acc
   playlist;
 }
